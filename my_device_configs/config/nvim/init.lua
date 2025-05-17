07c4092095721ee713e20e82fa5aeeac727cb72d
Nomix <145512477+Nomix17@users.noreply.github.com>
2025-05-17 22:21:57 +0000

    Update init.lua

--- my_device_configs/config/nvim/init.lua
+++ my_device_configs/config/nvim/init.lua
@@ -61,7 +61,7 @@ map('n', '<C-f>', ':NERDTreeFocus<CR>', opts)
 map('n', '<C-b>', ':NERDTreeToggle<CR>', opts)
 
 -- toggleterm mappings
-map('n','<C-j>', '<Cmd>exe v:count1 . "ToggleTerm"<CR>',{silent = true});
+-- map('n','<C-j>', '<Cmd>exe v:count1 . "ToggleTerm"<CR>',{silent = true});
 
 -- Additional mappings
 vim.keymap.set('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', { silent = true })
@@ -82,58 +82,4 @@ map('n', '<C-A-Up>', '<C-w>k', opts)
 map('n', '<C-A-Down>', '<C-w>j', opts)
 map('t', '<C-A-Up>', '<C-\\><C-n><C-w>k', opts)
 map('t', '<C-A-Down>', '<C-\\><C-n><C-w>j', opts)
-
--- Indent with Tab/Shift+Tab in visual mode
-map('x', '<Tab>', [[:s/^/    /<CR>gv]], opts)
-map('x', '<S-Tab>', [[:s/^    //<CR>gv]], opts)
-
--- Surround mappings
-for _, char in ipairs({"(", ")", "[", "]", "{", "}", "'", '"'}) do
-    map('x', char, '<ESC>>a' .. char .. '<ESC><i' .. char .. '<ESC>', opts)
-end
-
--- Autocommand to handle mapping conflicts
-local function setup_mappings()
-    if vim.fn.maparg('[', 'x') ~= '' then
-        vim.api.nvim_del_keymap('x', '[')
-    end
-end
-vim.api.nvim_create_autocmd("VimEnter", {
-    pattern = "*",
-    callback = setup_mappings
-})
-
--- Function to run code in a real terminal split
-function RunCode()
-    local filetype = vim.bo.filetype
-    local filename = vim.fn.expand("%:t") -- Get the current file name
-    local filepath = vim.fn.expand("%:p:h") -- Get the directory of the file
-    local command = ""
-
-    -- Set the command based on file type
-    if filetype == "python" then
-        command = "cd " .. filepath .. " && python " .. filename
-    elseif filetype == "cpp" then
-        command = "cd " .. filepath .. " && ./run.sh"
-    elseif filetype == "c" then
-        command = "cd " .. filepath .. " && gcc " .. filename .. " -o code && ./code"
-    else
-        print("No run command set for file type: " .. filetype)
-        return
-    end
-
-    -- Open a real terminal in a horizontal split
-    vim.cmd("belowright split | resize 10 | terminal")
-
-    -- Hide line numbers in the terminal
-    vim.cmd("setlocal nonumber norelativenumber")
-
-    -- Send the command inside the terminal
-    vim.defer_fn(function()
-        vim.api.nvim_feedkeys("i" .. command .. "\n", "n", true)
-    end, 100)
-end
-
--- Map Ctrl+Alt+N to run the function
-map('n', '<C-A-n>', ':lua RunCode()<CR>', opts)
 
