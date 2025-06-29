4e8dfb9d15179bb17aa01ff08f0310459139b086
izayrit <145512477+Nomix17@users.noreply.github.com>
2025-06-29 07:37:14 +0000

    Update Librewolf_Auto.sh

--- my_device_configs/config/custom-scripts/Librewolf_Auto.sh
+++ my_device_configs/config/custom-scripts/Librewolf_Auto.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-
 librewolf &
 
-for i in {1..20}; do
+while [ 1 ]; do
     wid=$(wmctrl -l | grep "LibreWolf" | awk '{print $1}')
-    
     if [[ -n "$wid" ]]; then
+        sleep 0.05
         i3-msg "[id=\"$wid\"] move scratchpad"
         exit 0
     fi
     
-    sleep 0.5
+    sleep 0.01
 done
 
 echo "LibreWolf window not found after waiting"
