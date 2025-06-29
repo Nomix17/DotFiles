b4d8191ad9ed944936a5380b2c27ddf0e320bc13
izayrit <145512477+Nomix17@users.noreply.github.com>
2025-06-29 07:47:52 +0000

    Update install_packages.sh

--- my_device_configs/install_packages.sh
+++ my_device_configs/install_packages.sh
@@ -1,1 +1,1 @@
-librewolf alacritty rofi nautilus lxappearance flatpak polybar neofetch neovim eog dunst picom
+librewolf alacritty rofi nautilus lxappearance flatpak polybar neofetch neovim eog dunst picom i3-autolayout
