762b5eff0841cf0df299203c198596eec59d4455
izayrit <145512477+Nomix17@users.noreply.github.com>
2025-07-26 10:14:49 +0000

    Update init.lua

--- my_device_configs/config/nvim/init.lua
+++ my_device_configs/config/nvim/init.lua
@@ -13,8 +13,8 @@ vim.opt.clipboard = "unnamedplus"
 -- Packer plugin manager setup
 vim.cmd [[packadd packer.nvim]]
 require('packer').startup(function()
-    use 'wbthomason/packer.nvim'  -- Packer manager
-    use 'catppuccin/nvim'          -- Additional theme option
+    use 'wbthomason/packer.nvim'
+    use 'catppuccin/nvim'
     use 'tpope/vim-surround'
     use 'neoclide/coc.nvim' 
     use 'tpope/vim-commentary'
@@ -31,6 +31,8 @@ require('packer').startup(function()
     use 'mg979/vim-visual-multi'
     use 'rainglow/vim'
     use 'petertriho/nvim-scrollbar'
+    use 'lukas-reineke/indent-blankline.nvim'
+
     use {"akinsho/toggleterm.nvim", tag = '*', config = function()
   require("toggleterm").setup()
 end}
@@ -45,38 +47,36 @@ vim.g.airline_theme = 'ouo'
 vim.keymap.set('n', '<C-S-j>', '5j', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-S-k>', '5k', { noremap = true, silent = true })
 
-
-
 -- Key mappings
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
-
--- Clipboard shortcuts
-map('v', '<C-S-X>', '"+x', opts)
-map('v', '<C-S-C>', '"+y', opts)
-map('v', 'y', '"+y', { noremap = true, silent = true })
-map('n', 'Y', '"+yy', { noremap = true, silent = true })
 
 -- NERDTree mappings
 map('n', '<C-f>', ':NERDTreeFocus<CR>', opts)
 map('n', '<C-b>', ':NERDTreeToggle<CR>', opts)
 
--- toggleterm mappings
--- map('n','<C-j>', '<Cmd>exe v:count1 . "ToggleTerm"<CR>',{silent = true});
-
 -- Additional mappings
-vim.keymap.set('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', { silent = true })
-vim.keymap.set('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', { silent = true })
-vim.keymap.set('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', { silent = true })
-vim.keymap.set('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', { silent = true })
-vim.keymap.set('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', { silent = true })
-vim.keymap.set('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', { silent = true })
-vim.keymap.set('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', { silent = true })
-vim.keymap.set('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', { silent = true })
-vim.keymap.set('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', { silent = true })
-vim.keymap.set('n', '<A-0>', '<Cmd>BufferLast<CR>', { silent = true })
-vim.keymap.set('n', '<C-w>', '<Cmd>BufferClose<CR>', { silent = true })
-vim.keymap.set('n', '<C-S-t>', '<Cmd>BufferRestore<CR>', { silent = true })
+vim.keymap.set('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', { silent = true, nowait = true  })
+vim.keymap.set('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', { silent = true, nowait = true  })
+vim.keymap.set('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', { silent = true, nowait = true  })
+vim.keymap.set('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', { silent = true, nowait = true  })
+vim.keymap.set('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', { silent = true, nowait = true  })
+vim.keymap.set('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', { silent = true, nowait = true  })
+vim.keymap.set('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', { silent = true, nowait = true  })
+vim.keymap.set('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', { silent = true, nowait = true })
+vim.keymap.set('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', { silent = true, nowait = true  })
+vim.keymap.set('n', '<A-0>', '<Cmd>BufferLast<CR>', { silent = true, nowait = true  })
+vim.keymap.set('n', '<A-d>', '<Cmd>BufferClose<CR>', { silent = true, nowait = true })
+vim.keymap.set('n', '<A-S-D>', '<Cmd>BufferRestore<CR>', { silent = true, nowait = true })
+
+vim.keymap.set('n', '<A-,>', '<Cmd>BufferPrevious<CR>', { silent = true, nowait = true })
+vim.keymap.set('n', '<A-.>', '<Cmd>BufferNext<CR>', { silent = true, nowait = true })
+
+vim.keymap.set('n', '<A-S-,>', '<Cmd>BufferMovePrevious<CR>', { silent = true, nowait = true })
+vim.keymap.set('n', '<A-S-.>', '<Cmd>BufferMoveNext<CR>', { silent = true, nowait = true })
+
+vim.keymap.set('n', '<A-p>', '<Cmd>BufferPick<CR>', { silent = true, nowait = true })
+vim.keymap.set('n', '<A-S-p>', '<Cmd>BufferPickDelete<CR>', { silent = true, nowait = true })
 
 map('n', '<C-a>', 'ggVG', opts)
 map("n", "<", "<<", opts)
@@ -97,3 +97,22 @@ require'barbar'.setup {
   
   clickable = true,
 }
+
+local hooks = require "ibl.hooks"
+
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "IndentBlanklineChar", { fg = "#474646" })
+end)
+
+require("ibl").setup({
+  indent = {
+    char = "┊",
+    highlight = { "IndentBlanklineChar" }
+  },
+  scope = {
+    enabled = true,
+    show_start = true,
+    show_end = false,
+  },
+})
+
